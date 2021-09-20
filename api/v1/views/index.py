2fe4ccac3adab862509b9e74e46d826ab841b94e
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-09-19 19:20:46 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """from"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """Returns status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Returns stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
