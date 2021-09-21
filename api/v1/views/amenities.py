f5408393928016b899106c36e1c9b57c85a7bde5
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-09-20 21:11:17 -0500

    test6

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""RestFul API actions amenities
+"""
+
+from models.amenity import Amenity
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def amenities_():
+    """ Retrieves list of amenities """
+    amenit = storage.all(Amenity).values()
+    amenities_list = []
+    for amenity in amenities:
+        amenities_list.append(amenity.to_dict())
+    return jsonify(amenities_list)
+
+
+@app_views.route('/amenities/<amenity_id>/', methods=['GET'],
+                 strict_slashes=False)
+def get_amenities_id(amenity_id):
+    """ Retrieves an specific amenity """
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenities(amenity_id):
+    """ Deletes an amenity by id """
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenities():
+    """ Create an amenities and return ir with status code 201 """
+
+    request_ = request.get_json()
+
+    if not request_():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+
+    if "name" not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+
+    new_ = Amenity(**data)
+    instance.save()
+    return make_response(jsonify(new_.to_dict()), 201)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_amenities(amenity_id):
+    """ updates an amenity object """
+
+    request_ = request.get_json()
+    amenity = storage.get(Amenity, amenity_id)
+
+    if not request_:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+
+    ignore = ['id', 'created_at', 'updated_at']
+    if not amenity:
+        abort(404)
+
+    for key, value in data.items():
+        if key not in key_ignore:
+            setattr(amenity, key, value)
+    storage.save()
+    return make_response(jsonify(amenity.to_dict()), 200)
